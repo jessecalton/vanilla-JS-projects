951ed24d89f93f5ad83eeaade55de550cd3a0130
Jesse Calton <jessecalton@gmail.com>
2020-10-19 22:57:19 -0700

    finishes exchange rate calc

--- exchange-rate/script.js
+++ exchange-rate/script.js
@@ -7,4 +7,34 @@ const amountEl_two = document.getElementById('amount-two');
 const rateEl = document.getElementById('rate');
 const swap = document.getElementById('swap');
 
-function calculate() {}
+// Fetch exchange rates and update the DOM
+function calculate() {
+  const currency_one = currencyEl_one.value;
+  const currency_two = currencyEl_two.value;
+
+  fetch(
+    `https://v6.exchangerate-api.com/v6/2acc6571da07af0d17764523/latest/${currency_one}`
+  )
+    .then((res) => res.json())
+    .then((data) => {
+      const rate = data.conversion_rates[currency_two];
+      rateEl.innerText = `1 ${currency_one} = ${rate} ${currency_two}`;
+      amountEl_two.value = (amountEl_one.value * rate).toFixed(2);
+    });
+}
+
+// Event listeners
+currencyEl_one.addEventListener('change', calculate);
+// `input` runs whenever the input changes
+amountEl_one.addEventListener('input', calculate);
+currencyEl_two.addEventListener('change', calculate);
+amountEl_two.addEventListener('input', calculate);
+
+swap.addEventListener('click', () => {
+  const temp = currencyEl_one.value;
+  currencyEl_one.value = currencyEl_two.value;
+  currencyEl_two.value = temp;
+  calculate();
+});
+
+calculate();
