23bd24588560ca6e553fb503f7452fe3ca6728ea
Jesse Calton <jessecalton@gmail.com>
2020-10-20 19:44:16 -0700

    finishes script.js

--- dom-array-methods/script.js
+++ dom-array-methods/script.js
@@ -9,6 +9,8 @@ const calculateWealthBtn = document.getElementById('calculate-wealth');
 // Each person has a name and money value.
 let data = [];
 getRandomUser();
+getRandomUser();
+getRandomUser();
 // fetch random user and add moneys
 async function getRandomUser() {
   const res = await fetch('https://randomuser.me/api');
@@ -23,7 +25,78 @@ async function getRandomUser() {
   addData(newUser);
 }
 
+// Doubles everyone's moneys
+function doubleMoney() {
+  data = data.map((user) => {
+    return { ...user, money: user.money * 2 };
+  });
+
+  updateDOM();
+}
+
+// Descending order of moneys
+function sortByRichest() {
+  data = data.sort((a, b) => {
+    return b.money - a.money;
+  });
+
+  updateDOM();
+}
+
+// Filter only millionaires
+function showMillionaires() {
+  data = data.filter((user) => {
+    return user.money >= 1000000;
+  });
+  updateDOM();
+}
+
+// Calculate the total wealth
+function calculateWealth() {
+  const wealth = data.reduce((acc, user) => {
+    return acc + user.money;
+  }, 0);
+  const wealthEl = document.createElement('div');
+  wealthEl.innerHTML = `<h3>Total Wealth: <strong>${formatMoney(
+    wealth
+  )}</strong>`;
+  main.appendChild(wealthEl);
+}
+
 // Add new obj to data arr
 function addData(obj) {
   data.push(obj);
+
+  updateDOM();
 }
+
+// Update DOM
+// default value is the data array
+function updateDOM(providedData = data) {
+  // Clear main div
+  main.innerHTML = '<h2><strong>Person</strong> Wealth</h2>';
+  providedData.forEach((item) => {
+    // Doing the same thing we did for the `person` and `wealth` text, just
+    // with our people data
+    const element = document.createElement('div');
+    element.classList.add('person');
+    element.innerHTML = `<strong>${item.name}</strong> ${formatMoney(
+      item.money
+    )}`;
+    // Appending it to the DOM
+    main.appendChild(element);
+  });
+}
+
+// Format number as money
+// Format number as money - https://stackoverflow.com/questions/149055/how-to-format-numbers-as-currency-string
+function formatMoney(number) {
+  return '$' + number.toFixed(2).replace(/\d(?=(\d{3})+\.)/g, '$&,');
+}
+
+//Event listeners
+addUserBtn.addEventListener('click', getRandomUser);
+doubleBtn.addEventListener('click', doubleMoney);
+sortBtn.addEventListener('click', sortByRichest);
+showMillionairesBtn.addEventListener('click', showMillionaires);
+calculateWealthBtn.addEventListener('click', calculateWealth);
